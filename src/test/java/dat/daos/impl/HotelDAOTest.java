b3ffd3bc0e2e9c9af30ed8693b642ffb8d835da8
peterjanas <pejanas@gmail.com>
2024-10-02 10:10:34 +0200

    hoteldaotest class

---
+++ src/test/java/dat/daos/impl/HotelDAOTest.java
@@ -0,0 +1,48 @@
+package dat.daos.impl;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HotelDAOTest
+{
+    @BeforeAll
+    static void setUpAll()
+    {
+    }
+
+    @BeforeEach
+    void setUp()
+    {
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+    }
+
+    @AfterAll
+    static void tearDownAll()
+    {
+    }
+
+    @Test
+    void readAll()
+    {
+    }
+
+    @Test
+    void create()
+    {
+    }
+
+    @Test
+    void update()
+    {
+    }
+
+    @Test
+    void delete()
+    {
+    }
+}
